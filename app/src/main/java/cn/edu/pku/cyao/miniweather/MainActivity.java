0d9f1d2c25021192f8b0d9a26bcf9ec88c72fcb6
YaoC <chengyao09@hotmail.com>
2016-09-27 20:37:23 +0800

    获取特定城市的天气数据

--- app/src/main/java/cn/edu/pku/cyao/miniweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/cyao/miniweather/MainActivity.java
@@ -1,16 +1,107 @@
 package cn.edu.pku.cyao.miniweather;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.zip.GZIPInputStream;
+
+import cn.edu.pku.cyao.util.NetUtil;
 
 /**
  * Created by cyao on 16-9-21.
+ * 1.创建Activity类（MainActivity）
+ * 2.创建layout布局文件 （weather_info.xml）
+ * 3.在AndroidManifest中注册创建的Activity类 （AndroidManifest.xml）
  */
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity implements View.OnClickListener{
+
+
+    private ImageView mUpdateBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        //加载布局
         setContentView(R.layout.weather_info);
+        mUpdateBtn = (ImageView) findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(this);
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu){
+//        getMenuInflater().inflate(R.menu.menu_main,menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        int id = item.getItemId();
+//        if(id==R.id.action_settings){
+//            return true;
+//        }
+//    }
+
+    @Override
+    public void onClick(View view){
+        if(view.getId()==R.id.title_update_btn){
+            SharedPreferences sharedPreferences = getSharedPreferences("config",MODE_PRIVATE);
+            String cityCode = sharedPreferences.getString("main_city_code","101010100");
+            Log.d("myWeather",cityCode);
+            if(NetUtil.getNetworkState(this)!=NetUtil.NETWORN_NONE){
+                Log.d("myWeather","网络OK");
+                queryWeatherCode(cityCode);
+            }else{
+                Log.d("myWeather","网络挂了");
+                Toast.makeText(MainActivity.this,"网络挂了！",Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+
+    private void queryWeatherCode(String cityCode){
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
+        Log.d("myWeather",address   );
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(address);
+                    URLConnection connection = url.openConnection();
+                    HttpURLConnection urlConnection = (HttpURLConnection) connection;
+                    urlConnection.connect();
+                    if(200==urlConnection.getResponseCode()){
+                        InputStream responseStream = urlConnection.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(responseStream));
+                        StringBuilder response = new StringBuilder();
+                        String str;
+                        while ((str=reader.readLine())!=null){
+                            response.append(str);
+                        }
+                        String responseStr = response.toString();
+                        Log.d("myWeather",responseStr);
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
